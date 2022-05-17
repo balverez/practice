42f40cf966341667cde2d377c658b2e5fb3f5e3c
brianaspie <91704138+brianaspie@users.noreply.github.com>
2022-05-16 23:15:41 -0700

    modified solution to the binary tree node distance problem.

--- c#/BinaryTreeNodeDistance/BinaryTreeNodeDistance/Solution.cs
+++ c#/BinaryTreeNodeDistance/BinaryTreeNodeDistance/Solution.cs
@@ -8,20 +8,20 @@ namespace BinaryTreeNodeDistance
 {
     internal class Solution
     {
-        internal int NodeDistance(TreeNode root, int target)
-        {
-            return RecursiveSearch(root, target, 1);
-        }
+        internal int NodeDistance(TreeNode root, int target) => DistanceDfs(root, target, 1);
 
-        private int RecursiveSearch(TreeNode? root, int target, int distance)
+        private int DistanceDfs(TreeNode? root, int target, int distance)
         {
             if (root == null)
                 return int.MaxValue;
 
             if (root.Data == target)
                 return distance;
-            else
-                return Math.Min(RecursiveSearch(root.Left, target, distance + 1), RecursiveSearch(root.Right, target, distance + 1));
+
+            distance++;
+            return Math.Min(
+                DistanceDfs(root.Left, target, distance),
+                DistanceDfs(root.Right, target, distance));
         }
     }
 }
