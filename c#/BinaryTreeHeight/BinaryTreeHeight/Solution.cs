f5a754f41b24e665a7dd8e7e5f358e747c4e6fc8
brianaspie <91704138+brianaspie@users.noreply.github.com>
2022-05-19 15:00:41 -0700

    modified c# solution to the binary tree height problem.

--- c#/BinaryTreeHeight/BinaryTreeHeight/Solution.cs
+++ c#/BinaryTreeHeight/BinaryTreeHeight/Solution.cs
@@ -4,6 +4,8 @@ namespace BinaryTreeHeight
 {
     internal class Solution
     {
+        //O(n) time
+        //O(n) space
         internal int GetHeight(TreeNode? root) => root == null
             ? 0
             : 1 + Math.Max(GetHeight(root.Left), GetHeight(root.Right));
