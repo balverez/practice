13af547f59de285fd9d68013726827bf5536b97d
brianaspie <91704138+brianaspie@users.noreply.github.com>
2022-05-21 21:39:32 -0700

    modified solution to the mutate dna pairs problem.

--- c#/MutateDnaPairs/MutateDnaPairs/Solution.cs
+++ c#/MutateDnaPairs/MutateDnaPairs/Solution.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MutateDnaPairs
+﻿namespace MutateDnaPairs
 {
     internal class Solution
     {
+        //O(n) time
+        //O(n) space
         public string MutateDnaPairs(string sequence)
         {
             if (sequence.Length < 2)
                 return sequence;
 
-
-            return sequence[0]
-                + (sequence[0] == sequence[1] ? "G" : string.Empty)
-                + MutateDnaPairs(sequence[1..]);
+            return sequence[0] == sequence[1]
+                ? sequence[0] + "G" + MutateDnaPairs(sequence[1..])
+                : sequence[0] + MutateDnaPairs(sequence[1..]);
         }
     }
 }
